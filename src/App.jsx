bb2c8a77c9db36ebe92068dc210a8511631a2ea1
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2023-09-26 23:04:50 +0530

    Created logic for getting the current user

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import './App.css'
+import { useEffect } from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/authService";
+import { login, logout } from "./slices/authSlice";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      Hello World
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+  !loading ? <div>This is test</div> : null;
 }
 
-export default App
+export default App;
